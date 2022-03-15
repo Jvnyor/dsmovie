0aa886661dff568f7dcde836338ef234170c3c2e
Jvnyor <josias100junior@gmail.com>
2022-03-14 22:40:41 -0300

    Find movies

---
+++ backend/src/main/java/com/Jvnyor/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,11 @@
+package com.Jvnyor.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Jvnyor.dsmovie.entities.Movie;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+}
